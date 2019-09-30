d43c015b796e78255bebf740098833e5fcc6b2a1
Christian <newera.cabal@hotmail.com>
2019-09-30 16:22:02 -0300

    fix

--- HistogramExerc1.py
+++ HistogramExerc1.py
@@ -3,24 +3,25 @@ import numpy as np
 from PIL import Image
 import math
 
-
 arquive = 'm.jpeg'
 
-tons = 256
-image = cv2.imread(arquive, cv2.IMREAD_GRAYSCALE)
+def convertAndSave(array, name):
+    img = Image.fromarray(array)
+    if img.mode != 'RGB':
+        img = img.convert('RGB')
+    img.save('{}.png'.format(name))
 
+image = cv2.imread(arquive, cv2.IMREAD_GRAYSCALE)
 imgOri = Image.fromarray(image)
 imgOri.save('GreyScale.png')
 
+tons = 256
 n = len(image[0]) * len(image)
-
 b = 0.5
 c = 255 / math.log(256)
-
 soma = 0
 hist = cv2.calcHist([image], [0], None, [256], [0, 256])
 hist = hist.astype(int)
-histManual = []
 histAcul = []
 pk = []
 KLinha = []
@@ -37,7 +38,6 @@ for a in range(len(hist)):
     pk.append(histAcul[a]/n)
     KLinha.append(np.around((tons-1)*pk[a]))
 
-
 KLinha = np.array(KLinha)
 KLinha = KLinha.astype(int)
 
@@ -49,23 +49,8 @@ for idx_linha, val_linha in enumerate(image):
             math.log(1 + image[idx_linha][index])
         poten_img[idx_linha][index] = (c * image[idx_linha][index]) ** b
 
-img = Image.fromarray(equal_img)
-if img.mode != 'RGB':
-    img = img.convert('RGB')
 
-imgNeg = Image.fromarray(negat_img)
-if imgNeg.mode != 'RGB':
-    imgNeg = imgNeg.convert('RGB')
-
-imgLogarit = Image.fromarray(logarit_img)
-if imgLogarit.mode != 'RGB':
-    imgLogarit = imgLogarit.convert('RGB')
-
-imgPoten = Image.fromarray(poten_img)
-if imgPoten.mode != 'RGB':
-    imgPoten = imgPoten.convert('RGB')
-
-img.save('Equalize.png')
-imgNeg.save('Negative.png')
-imgLogarit.save('Logarit.png')
-imgPoten.save('potencia.png')
+convertAndSave(equal_img, 'Equalize')
+convertAndSave(negat_img, 'Negative')
+convertAndSave(logarit_img, 'Logarit')
+convertAndSave(poten_img, 'Potenc')
